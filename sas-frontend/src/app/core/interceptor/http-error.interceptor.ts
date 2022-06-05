cbe9030cf28750f03c1b06e2f01c250199a72d0f
Luiz Gustavo <gustavo_x@live.com>
2022-06-05 12:18:51 -0300

    interceptor

---
+++ sas-frontend/src/app/core/interceptor/http-error.interceptor.ts
@@ -0,0 +1,25 @@
+import {
+  HttpEvent, HttpHandler, HttpInterceptor, HttpRequest
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { SweetAlertService } from '../service/sweet-alert.service';
+
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
+
+  constructor(private sweetAlert: SweetAlertService){}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError(e => {
+        
+        this.sweetAlert.error(e?.error?.message)
+
+        return throwError(e.error)
+      })
+    )
+  }
+  
+}
